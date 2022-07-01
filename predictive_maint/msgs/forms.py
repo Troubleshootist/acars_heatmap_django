571635c7e691ae8469b1823ade4fa20c28cb0c47
Dmitriy Sidorov <d.sidorov@airbridgecargo.com>
2022-07-01 13:50:10 +0300

    ---

--- predictive_maint/msgs/forms.py
+++ predictive_maint/msgs/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 
+
 class OccurrencesDataRangeForm(forms.Form):
     """Form representing a range of date"""
-    from_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
-    to_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+    from_date = forms.DateField(widget=forms.DateInput(
+        attrs={'type': 'date', 'class': 'form-control'}))
+    to_date = forms.DateField(widget=forms.DateInput(
+        attrs={'type': 'date', 'class': 'form-control'}))
+    
